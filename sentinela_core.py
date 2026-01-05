3decda5907953b981d359555eb08cde0faecd1e4
mariibrb <marii.brbj@gmail.com>
2026-01-05 11:22:29 -0300

    Update sentinela_core.py

--- sentinela_core.py
+++ sentinela_core.py
@@ -39,33 +39,28 @@ def extrair_dados_xml(files):
                             if cst is not None: linha["CST_ICMS"] = cst.text.zfill(2)
                             if n.find('vBC') is not None: linha["BC_ICMS"] = float(n.find('vBC').text)
                             if n.find('vICMS') is not None: linha["VLR_ICMS"] = float(n.find('vICMS').text)
-                    
                     vIpi = imp.find('.//IPI/vIPI')
                     if vIpi is not None: linha["VLR_IPI"] = float(vIpi.text)
                     vPis = imp.find('.//PIS//vPIS')
                     if vPis is not None: linha["VLR_PIS"] = float(vPis.text)
                     vCofins = imp.find('.//COFINS//vCOFINS')
                     if vCofins is not None: linha["VLR_COFINS"] = float(vCofins.text)
-                    
                 dados_lista.append(linha)
         except: continue
     return pd.DataFrame(dados_lista)
 
-def gerar_excel_final(df_xe, df_xs, b_unica, ae, as_f, ge, gs, cod_cliente=""):
+def gerar_excel_final(df_xe, df_xs, b_unica, ge, gs, cod_cliente=""):
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         if not df_xe.empty: df_xe.to_excel(writer, sheet_name='XML_ENTRADAS', index=False)
         if not df_xs.empty: df_xs.to_excel(writer, sheet_name='XML_SAIDAS', index=False)
-        
         if ge:
             try: pd.read_csv(ge, sep=None, engine='python').to_excel(writer, sheet_name='GERENCIAL_ENTRADA', index=False)
             except: pass
         if gs:
             try: pd.read_csv(gs, sep=None, engine='python').to_excel(writer, sheet_name='GERENCIAL_SAIDA', index=False)
             except: pass
-
         if b_unica is not None:
             try: pd.read_excel(b_unica).to_excel(writer, sheet_name='BASE_AUDITORIA', index=False)
             except: pass
-            
     return output.getvalue()
